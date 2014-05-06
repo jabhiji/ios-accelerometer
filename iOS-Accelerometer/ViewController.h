bd0c80d8a98442553d00b9d0b29542ccf6e83911
Abhijit Joshi <joshi1974@gmail.com>
2014-05-06 02:41:48 -0400

    the code checks if the accelerometer is available for use

--- iOS-Accelerometer/ViewController.h
+++ iOS-Accelerometer/ViewController.h
@@ -7,7 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController : UIViewController
+
+// acceleration values
+@property double currentAccX;
+@property double currentAccY;
+@property double currentAccZ;
+
+// labels
+@property (strong, nonatomic) IBOutlet UILabel *accX;
+@property (strong, nonatomic) IBOutlet UILabel *accY;
+@property (strong, nonatomic) IBOutlet UILabel *accZ;
+
+// check if accelerometer is available
+@property(readonly, nonatomic, getter=isAccelerometerAvailable) BOOL accelerometerAvailable;
+
+// motion manager
+@property (strong, nonatomic) CMMotionManager *motionManager;
+
+
+- (void) checkForAccelerometer;
 
 @end
--- iOS-Accelerometer/ViewController.m
+++ iOS-Accelerometer/ViewController.m
@@ -14,10 +14,40 @@
 
 @implementation ViewController
 
+@synthesize accelerometerAvailable;
+@synthesize currentAccX;
+@synthesize currentAccY;
+@synthesize currentAccZ;
+@synthesize accX;
+@synthesize accY;
+@synthesize accZ;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self checkForAccelerometer];
+    
+    currentAccX = 0.0;
+    currentAccY = 0.0;
+    currentAccZ = 0.0;
+
+    // initialize motion manager
+    self.motionManager = [[CMMotionManager alloc] init];
+    self.motionManager.accelerometerUpdateInterval = .2;
+    self.motionManager.gyroUpdateInterval = .2;
+
+    
+}
+
+- (void) checkForAccelerometer
+{
+    if (accelerometerAvailable) {
+        NSLog(@"Yes, this device has an accelerometer");
+    } else {
+        NSLog(@"No accelerometer is available!");
+    }
 }
 
 - (void)didReceiveMemoryWarning
